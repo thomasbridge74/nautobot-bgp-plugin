646d5d2d9a6ba89611fcfba049faf671f54310f3
Thomas Bridge <thomas.bridge@icloud.com>
2022-04-15 13:31:29 +0100

    Updated to reflect fork.

--- nautobot_bgp_plugin/__init__.py
+++ nautobot_bgp_plugin/__init__.py
@@ -1,18 +1,18 @@
-from extras.plugins import PluginConfig
+from nautobot.extras.plugins import PluginConfig
 from .version import __version__
 
 
 class BGPConfig(PluginConfig):
     name = "nautobot_bgp_plugin"
     verbose_name = "BGP"
-    description = "Subsystem for tracking bgp related objects"
+    description = "Subsystem for tracking bgp related objects (forked from Netbox Plugin)"
     version = __version__
-    author = "Nikolay Yuzefovich"
-    author_email = "mgk.kolek@gmail.com"
+    author = "Thomas Bridge (forked from Nikolay Yuzefovich)"
+    author_email = "thomas.bridge@icloud.com"
     base_url = "bgp"
     required_settings = []
-    min_version = "2.10.1"
-    max_version = "2.11.12"
+    # min_version = "2.10.1"
+    # max_version = "2.11.12"
     default_settings = {"device_ext_page": "right"}
 
 
